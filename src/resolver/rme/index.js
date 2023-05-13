6dc672ca0f90ad06956f26133c633d2d3676e7d6
milhamap <ilhamap45@gmail.com>
2023-05-13 17:24:45 +0700

    Update user_id not found

--- src/resolver/rme/index.js
+++ src/resolver/rme/index.js
@@ -108,9 +108,8 @@ module.exports = {
             const validate = v.validate(req.query, schema)
             if (validate.length) return res.status(400).json({message: validate})
             const result = await knex('rme').where({nomor}).first()
-            const user = await knex('users').where({id: result.user_id}).first()
-            console.log(result)
             if (!result) return res.status(404).json({message: 'RME not found'})
+            const user = await knex('users').where({id: result.user_id}).first()
             return res.status(200).json({
                 message: 'RME success vertifikasi',
                 data: {
