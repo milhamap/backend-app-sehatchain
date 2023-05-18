c8b3390abb5ae321b2cc9b03709670b145f8f067
milhamap <ilhamap45@gmail.com>
2023-05-18 08:44:38 +0700

    Update blockhash

--- src/resolver/auth/index.js
+++ src/resolver/auth/index.js
@@ -31,7 +31,7 @@ module.exports = {
                 address: 'string|empty:false',
                 email: 'email|empty:false',
                 nik: 'string|empty:false|min:16|max:16',
-                publicAddress: 'string|empty:true',
+                publicAddress: 'string|empty:false',
             }
             const validate = v.validate({
                 fullname,
@@ -97,7 +97,7 @@ module.exports = {
                 phone: 'string|empty:false|min:10|max:13',
                 address: 'string|empty:false',
                 email: 'email|empty:false',
-                // publicAddress: 'string|empty:true',
+                publicAddress: 'string|empty:false',
                 key: 'string|empty:false',
             }
             const validate = v.validate({
@@ -105,7 +105,7 @@ module.exports = {
                 phone,
                 address,
                 email,
-                // publicAddress,
+                publicAddress,
                 key
             }, schema)
             if (validate.length) return res.status(400).json({message: validate})
@@ -125,7 +125,7 @@ module.exports = {
                 email, 
                 address,
                 phone,
-                public_address: "",
+                public_address: publicAddress,
                 random,
                 role_id: 2,
                 created_at: new Date(),
